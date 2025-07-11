b6ca4e269df17056d1e5475bfd9f3887aa8d4603
S0MBRA99 <167691743+S0MBRA99@users.noreply.github.com>
2025-07-11 20:26:27 +0000

    cambios

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
 import "bootstrap";
-import "./style.css";
+import { Button } from "bootstrap";
 
+let listOfSentence = [
+"Gyomei es ciego desde pequeño, pero percibe todo con increíble precisión.",
+"Es el más fuerte físicamente de todos los Pilares.",
+"Siempre lleva cuentas budistas y reza incluso en batalla.",
+"A pesar de su aspecto, es muy sensible y llora fácilmente.",
+"Usa una enorme hacha con una bola de hierro unida por cadena.",
+"Se entrenó en un templo cuidando huérfanos antes de ser cazador.",
+"Puede activar la Marca del Cazador de forma consciente.",
+"Su respiración es la del Pilar de Piedra, una de las más poderosas.",
+]
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let contador = 0
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+document.getElementById("buttton-curiosidades").addEventListener("click",funcionJS)
+  
+function funcionJS(){
+  const parrafo = document.getElementById("respuesta-funcion")
+  
+  if(contador >= listOfSentence.length){
+    location.reload()
+    return
+  }
+  
+  parrafo.textContent = listOfSentence[contador]
+  contador += 1 
+}
+
